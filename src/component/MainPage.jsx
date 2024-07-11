2052ae20247fb7c5d4d6817f28e5d1a4f098ca56
ANKUR SASMAL <ankursasmal@ANKURs-MacBook-Air.local>
2024-07-11 19:11:43 +0530

    complete

--- src/component/MainPage.jsx
+++ src/component/MainPage.jsx
@@ -11,7 +11,7 @@ import { FiSearch } from "react-icons/fi";
 function MainPage() {
 
   return (
-    <div style={{ position:'absolute',top:'0',left:'16vw',width:'82%' }}>
+    <div style={{ position:'absolute',top:'0',left:'16vw',width:'82%',margin:'auto' }}>
       <div className={`${style.MainPgae}`}  >
         {/* nav part */}
         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
